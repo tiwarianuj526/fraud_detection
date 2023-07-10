eeb4bae1a3c277fd0894a9ee74d056149f4cbf0e
tiwarianuj526 <tiwarianuj526@gmail.com>
2023-07-10 23:40:11 +0530

    model2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,14 @@
 
 print("New file for all ML_model code")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model3():
+    print("adding code for model3")
+
+
+print("model 2 is performing the best")
